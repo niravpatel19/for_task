1431e50d67721cfb22461f64e76862ae26a93a7f
niravpatel19 <patelnirav6756@gmail.com>
2021-07-28 10:53:58 +0530

    msg

---
+++ newapi.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		<titel>covid_19</titel>
+		<link rel="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<link rel="use.fontawesome.com/releases/v5.0.7/css/all.css">
+	</head>
+	<body>
+			<div class="">
+	
+	</body>
+</html>
